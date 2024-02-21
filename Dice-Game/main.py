71dd12c36dd380ddfb3afbaae56f3022ae81dbc5
Jettapol Tuetrakul <buumu8@gmail.com>
2024-02-21 11:23:33 +0700

    apply formatter

--- Dice-Game/main.py
+++ Dice-Game/main.py
@@ -1,9 +1,10 @@
 from itertools import combinations
 
+
 def count_wins(dice1, dice2):
     assert len(dice1) == 6 and len(dice2) == 6
     dice1_wins, dice2_wins = 0, 0
-    
+
     # write your code here
 
     for d1 in dice1:
@@ -15,17 +16,18 @@ def count_wins(dice1, dice2):
 
     return (dice1_wins, dice2_wins)
 
+
 def find_the_best_dice(dices):
     assert all(len(dice) == 6 for dice in dices)
-    
+
     # write your code here
     # use your implementation of count_wins method if necessary
     max_win = -1
     winner = -1
     comb = combinations(range(len(dices)), 2)
-    
-    for d1,d2 in comb:
-        d1_wins, d2_wins = count_wins(dices[d1],dices[d2])
+
+    for d1, d2 in comb:
+        d1_wins, d2_wins = count_wins(dices[d1], dices[d2])
         # print(d1,d2,d1_wins,d2_wins,max_win,winner)
         if d1_wins > d2_wins and d1_wins > max_win:
             max_win = d1_wins
@@ -38,5 +40,6 @@ def find_the_best_dice(dices):
 
     return winner
 
-dices=  [[1, 1, 2, 4, 5, 7], [1, 2, 2, 3, 4, 7], [1, 2, 3, 4, 5, 6]]
+
+dices = [[1, 1, 2, 4, 5, 7], [1, 2, 2, 3, 4, 7], [1, 2, 3, 4, 5, 6]]
 print(find_the_best_dice(dices))

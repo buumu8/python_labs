6fdcea0466d0fa45aff33493f7ba2eaab188701e
Jettapol Tuetrakul <buumu8@gmail.com>
2024-09-20 11:18:51 +0700

    add django requests and urls lab

--- django-first-lab/project/myapp/urls.py
+++ django-first-lab/project/myapp/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("demo", views.index, name="index"),
+    path("main", views.main, name="main"),
+    path("header", views.header, name="header"),
+    path("getuser/<name>/<id>", views.pathview, name="pathview"),
+    path("getuser/", views.qryview, name="qryview"),
+    path("showform/", views.showform, name="showform"),
+    path("getform/", views.getform, name="getform"),
+    path("dishes/<str:dish>", views.menuitems),
 ]
--- django-first-lab/project/myapp/views.py
+++ django-first-lab/project/myapp/views.py
@@ -7,6 +7,33 @@ def home(request):
     return HttpResponse("Welcome to Little Lemon!")
 
 
+def main(request):
+    path = request.path
+    return HttpResponse(path, content_type="text/html", charset="utf-8")
+
+
+def header(request):
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META["REMOTE_ADDR"]
+    user_agent = request.META["HTTP_USER_AGENT"]
+    path_info = request.path_info
+
+    response = HttpResponse()
+    response.headers["Age"] = 20
+    msg = f"""<br>
+        <br>Path: {path}
+        <br>Address: {address}
+        <br>Sceme: {scheme}
+        <br>Method: {method}
+        <br>User agent: {user_agent}
+        <br>Path info: {path_info}
+        <br> Response header: {response.headers}
+        """
+    return HttpResponse(msg, content_type="text/html", charset="utf-8")
+
+
 def index(request):
     path = request.path
     method = request.method
@@ -20,6 +47,39 @@ def index(request):
         path, method
     )
     return HttpResponse(content)
+
+
+def pathview(request, name, id):
+    return HttpResponse("Name:{} UserID:{}".format(name, id))
+
+
+def qryview(request):
+    name = request.GET["name"]
+    id = request.GET["id"]
+    return HttpResponse("Name:{} UserID: {}".format(name, id))
+
+
+def showform(request):
+    return render(request, "form.html")
+
+
+def getform(request):
+    if request.method == "POST":
+        id = request.POST["id"]
+        name = request.POST["name"]
+    return HttpResponse("Name: {} UserID: {}".format(name, id))
+
+
+def menuitems(request, dish):
+    items = {
+        "pasta": "an italian dish",
+        "falafel": "an israeli dish",
+        "cheesecake": "a french dessert",
+    }
+
+    description = items[dish]
+
+    return HttpResponse(f"<h2> {dish} </h2>" + description)
 
 
 # source venv/Scripts/activate

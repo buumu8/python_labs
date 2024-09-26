3a5d118044d184b0a8123a4f997fd2390c78dced
Jettapol Tuetrakul <buumu8@gmail.com>
2024-09-26 11:08:05 +0700

    play around with model migrations

--- django-first-lab/project/myapp/models.py
+++ django-first-lab/project/myapp/models.py
@@ -8,6 +8,16 @@ class Menu(models.Model):
     cuisine = models.CharField(max_length=100)
     price = models.IntegerField()
 
-
     def __str__(self):
         return self.name + " : " + self.cuisine
+
+
+class Menuitems(models.Model):
+    itemname = models.CharField(max_length=200)
+    category = models.CharField(max_length=300)
+    year = models.IntegerField()
+
+
+class Drinks(models.Model):
+    drink_name = models.CharField(max_length=200)
+    price = models.IntegerField()

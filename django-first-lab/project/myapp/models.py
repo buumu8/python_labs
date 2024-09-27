fca65661becb983d46fa7205d67b58c48dc1658a
Jettapol Tuetrakul <buumu8@gmail.com>
2024-09-27 12:30:37 +0700

    test model foreign key

--- django-first-lab/project/myapp/models.py
+++ django-first-lab/project/myapp/models.py
@@ -3,10 +3,21 @@ from unicodedata import name
 
 
 # Create your models here.
+
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+
 class Menu(models.Model):
-    name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
-    price = models.IntegerField()
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(
+        MenuCategory,
+        on_delete=models.PROTECT,
+        default=None,
+        related_name="category_name",
+    )
 
     def __str__(self):
         return self.name + " : " + self.cuisine
